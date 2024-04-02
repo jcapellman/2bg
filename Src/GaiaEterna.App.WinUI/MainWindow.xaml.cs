c4b60ce83d6c7b594ddf9e4a141cf8ee77b45034
Jarred Capellman <jcapellman@hotmail.com>
2024-04-02 06:17:42 -0500

    Added: Full Screen Mode

--- Src/GaiaEterna.App.WinUI/MainWindow.xaml.cs
+++ Src/GaiaEterna.App.WinUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using GaiaEterna.App.WinUI.Helpers;
 
 using Windows.UI.ViewManagement;
+using Microsoft.UI.Windowing;
 
 namespace GaiaEterna.App.WinUI;
 
@@ -14,24 +15,19 @@ public sealed partial class MainWindow : WindowEx
     {
         InitializeComponent();
 
+        AppWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
 
-        // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
         dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
         settings = new UISettings();
-        settings.ColorValuesChanged += Settings_ColorValuesChanged; // cannot use FrameworkElement.ActualThemeChanged event
+        settings.ColorValuesChanged += Settings_ColorValuesChanged;
     }
 
-    // this handles updating the caption button colors correctly when indows system theme is changed
-    // while the app is open
     private void Settings_ColorValuesChanged(UISettings sender, object args)
     {
-        // This calls comes off-thread, hence we will need to dispatch it to current app's thread
-        dispatcherQueue.TryEnqueue(() =>
-        {
-            TitleBarHelper.ApplySystemThemeToCaptionButtons();
-        });
+        dispatcherQueue.TryEnqueue(TitleBarHelper.ApplySystemThemeToCaptionButtons);
     }
 }
