a1fae79230d06084b4362984e347e2d932cc9c0d
Jarred Capellman <jcapellman@hotmail.com>
2024-04-01 21:34:32 -0500

    Added: UWP Game

---
+++ Src/GaiaEterna.App.UWP/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace GaiaEterna.App.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
+        }
+
+        private void CcMain_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawText("Gaia Eterna", 100, 100, Colors.White);
+        }
+    }
+}
